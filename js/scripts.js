6d203b8b4b4def47cee169a5af107deb7ca37c1d
M. Hille <marciausgoslar@gmail.com>
2019-07-07 16:36:26 +0200

    Added pokemon class & basic pokemon repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-alert(message)('Hello World');
+function Pokemon(name, height, types) {
+  this.name = name;
+  this.height = height;
+  this.types = types;
+}
+
+// The container which contains all pokemons to display
+var repository = [
+  new Pokemon("Bulbasur", 7, ["grass", "poison"]),
+  new Pokemon("Sandslash", 1, ["Ground"]),
+  new Pokemon("Golduck", 1.7, ["water"])
+];
