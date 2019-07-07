4a77a3b90e8109f426704a0756e4ad9eec9b94ad
M. Hille <marciausgoslar@gmail.com>
2019-07-07 16:52:19 +0200

    Added pokemon display functionality & card view style

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
+// Container for storing pokemon relevant data
 function Pokemon(name, height, types) {
   this.name = name;
   this.height = height;
   this.types = types;
 }
 
-// The container which contains all pokemons to display
+// List which contains all pokemons to display
 var repository = [
   new Pokemon("Bulbasur", 7, ["grass", "poison"]),
   new Pokemon("Sandslash", 1, ["Ground"]),
   new Pokemon("Golduck", 1.7, ["water"])
 ];
+
+// Display all pokemons on webpage
+for (var pokemon of repository) {
+  var displayText = `${pokemon.name}, (height: ${pokemon.height})`;
+  
+  if(pokemon.height > 5)
+    displayText += ' - Wow, that\'s big!';
+  document.write(`<p>${displayText}</p>`);
+}
+
