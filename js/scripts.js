1c21791b864b581f85773ab6ca0d8f976e734776
M. Hille <marciausgoslar@gmail.com>
2019-07-12 09:41:39 +0200

    Added function to add pokemon cards to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // DOM Elements
-var $pokemonList = document.querySelector('.main-content_pokemon-list');
-
+var $pokemonList = document.querySelector(".main-content_pokemon-list");
 
 // Container for storing pokemon relevant data
 function Pokemon(name, height, types) {
@@ -31,8 +30,8 @@ var pokemonRepository = (function() {
 
   // Function to get all pokemons with the filter applied
   function getFiltered(filter) {
-    if(!filter) return [];
-    
+    if (!filter) return [];
+
     return repository.filter(c =>
       String(Object.values(c))
         .toLowerCase()
@@ -47,19 +46,86 @@ var pokemonRepository = (function() {
     repository.splice(repository.indexOf(pokemon), 1);
   }
 
+  // Function to add the pokemon card to the DOM
+  function renderPokemonCard(pokemonCard) {
+    if (!pokemonCard) return;
+
+    $pokemonList.appendChild(pokemonCard);
+  }
+
+  // Function to build pokemon cards
+  function getPokemonCards(pokemons) {
+    return pokemons.map(p => getPokemonCard(p));
+  }
+
+  // Function to build pokemon card
+  function getPokemonCard(pokemon) {
+    if (!isPokemon(pokemon)) return "";
+
+    return getAsListItem(pokemon);
+  }
+
+  // Function to create a UI element form pokemon data
+  function getAsListItem(pokemon) {
+    var listItem = document.createElement("li");
+    var itemButton = document.createElement("button");
+    itemButton.innerText = pokemon.name;
+    addButtonStyle(itemButton);
+    listItem.appendChild(itemButton);
+    return listItem;
+  }
+
+  // Function to add button styles
+  function addButtonStyle(listItem) {
+    listItem.classList.add("item_button");
+  }
+
+  // Function to validate an object as pokemon
+  function isPokemon(item) {
+    return isObject(item) && isObjectEqual(item, new Pokemon());
+  }
+
+  // Function to validate an item as object
+  function isObject(item) {
+    return item !== null && item !== undefined && typeof item === "object";
+  }
+
+  // Function to validate object equality
+  function isObjectEqual(original, clone) {
+    var originalProperties = Object.keys(original);
+    var cloneProperties = Object.keys(clone);
+
+    if (!isPropertyCountEqual(originalProperties, cloneProperties))
+      return false;
+
+    for (var i = 0; i < originalProperties.length; i++)
+      if (originalProperties[i] !== cloneProperties[i]) return false;
+
+    return true;
+  }
+
+  // Function to validate property count equality
+  function isPropertyCountEqual(originalProperties, cloneProperties) {
+    return originalProperties.length === cloneProperties.length;
+  }
+
   return {
     add: add,
     getAll: getAll,
     getFiltered: getFiltered,
-    remove: remove
+    getPokemonCards: getPokemonCards,
+    remove: remove,
+    renderPokemonCard: renderPokemonCard
   };
 })();
 
-// Function to display all pokemons on webpage
-function printPokemonDetails(pokemons) {
+// Function to add multiple pokemon cards to the DOM
+function renderPokemonCards(pokemons) {
   if (!isEnumeratorValid(pokemons)) return;
 
-  getPokemonCards(pokemons).forEach(c => $pokemonList.appendChild(c));
+  pokemonRepository
+    .getPokemonCards(pokemons)
+    .forEach(c => pokemonRepository.renderPokemonCard(c));
 }
 
 // Function to validate, if parameter is a valid enumerator
@@ -67,60 +133,4 @@ function isEnumeratorValid(enumerator) {
   return enumerator && Array.isArray(enumerator) && enumerator.length;
 }
 
-// Function to build pokemon cards
-function getPokemonCards(pokemons) {
-  return pokemons.map(p => getAsCard(p));
-}
-
-// Function to build pokemon card text
-function getAsCard(pokemon) {
-  if (!isPokemon(pokemon)) return '';
-
-  var listItem = getAsListItem(pokemon)
-  return listItem;
-}
-
-// Function to create a UI element form pokemon data
-function getAsListItem(pokemon) {
-  var listItem = document.createElement('li');
-  var itemButton = document.createElement('button');
-  itemButton.innerText = pokemon.name;
-  addButtonStyle(itemButton)
-  listItem.appendChild(itemButton);
-  return listItem;
-}
-
-// Function to add button styles
-function addButtonStyle(listItem) {
-  listItem.classList.add('item_button');
-}
-
-// Function to validate an object as pokemon
-function isPokemon(item) {
-  return isObject(item) && isObjectEqual(item, new Pokemon());
-}
-
-// Function to validate an item as object
-function isObject(item) {
-  return item !== null && item !== undefined && typeof item === "object";
-}
-
-// Function to validate object equality
-function isObjectEqual(original, clone) {
-  var originalProperties = Object.keys(original);
-  var cloneProperties = Object.keys(clone);
-
-  if (!isPropertyCountEqual(originalProperties, cloneProperties)) return false;
-
-  for (var i = 0; i < originalProperties.length; i++)
-    if (originalProperties[i] !== cloneProperties[i]) return false;
-
-  return true;
-}
-
-// Function to validate property count equality
-function isPropertyCountEqual(originalProperties, cloneProperties) {
-  return originalProperties.length === cloneProperties.length;
-}
-
-printPokemonDetails(pokemonRepository.getAll());
+renderPokemonCards(pokemonRepository.getAll());
